4780d668f7ca511e46fd0ff288c5664856da2ba9
Chloe Scheffler <chloescheffler@gmail.com>
2022-08-20 16:04:19 -0400

    commit tree 16e9b8b6cbe023f40c5479e64008f4e0d0e0870e

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Your code here
+// Move dodger left
+const dodger = document.getElementById("dodger");
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+
+// Move dodger right 
+function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    if (left < 360) {
+        dodger.style.left = `${left + 1}px`;
+    }
+}
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+});
